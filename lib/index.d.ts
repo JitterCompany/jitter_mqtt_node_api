e443cbfe346fdff26845e1cabafa8ecd7891da3f
Ingmar Jager <ingmarjager@gmail.com>
2018-10-23 14:41:16 +0200

    remove 'export interface MQTTMetaData' Not needed in client code

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,4 @@
-import { ProgressData } from './mqtt-metadata';
 export * from './api';
 export * from './mqtt.model';
 export { ProgressEventCallback, ProgressData } from "./mqtt-metadata";
-export interface MQTTMetaData {
-    progress: ProgressData;
-}
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,3 @@ import { ProgressData } from './mqtt-metadata';
 export * from './api';
 export * from './mqtt.model';
 export {ProgressEventCallback, ProgressData} from "./mqtt-metadata";
-
-export interface MQTTMetaData {
-  progress: ProgressData
-}
